3238069653a3371548c316f7cefa985d1107a35a
Tatiana Osipova <otv120780@gmail.com>
2023-06-09 10:58:25 -0400

    feat: start js

---
+++ js/main.js
@@ -0,0 +1,9 @@
+document.addEventListener('mousemove', e => {
+    Object.assign(document.documentElement, {
+        style: `
+        --move-x: ${e.clientX}deg;
+        --move-y: ${e.clientY}deg;
+        `
+
+    })
+})
